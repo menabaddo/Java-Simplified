0a584570502d73a43fa2c7357cdddc33b373d6f8
menabaddo <edmundalexfresh@gmail.com>
2022-08-06 18:31:28 +0100

    For loop

---
+++ src/for_loop/Main.java
@@ -0,0 +1,21 @@
+package for_loop;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// for loop executes a block of code a limited amount of times
+
+		// initialization, condition, increment
+
+		for (int x = 10; x >= 1;x--;) { // you could decrement here
+
+			System.out.println(x);
+			// // you could also decrement within the code
+
+		}
+
+		System.out.println("HAPPY NEW YEAR!!!");
+	}
+
+}
