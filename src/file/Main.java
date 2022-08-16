1f0e3a505f278461a28fed2767ea6de5e83b99b9
menabaddo <edmundalexfresh@gmail.com>
2022-08-16 15:52:21 +0100

    File

---
+++ src/file/Main.java
@@ -0,0 +1,30 @@
+package file;
+
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// file = An abstract representation of file and directory pathnames
+
+		File file = new File("C:\\Users\\mmm\\Desktop\\secret_message.txt"); // passing in the name of the file as an
+																				// argument
+		// if the file is located outside the project
+		// use the file path
+
+		// if within the project just use the file name.
+
+		if (file.exists()) {
+			System.out.println("File exist");
+			System.out.println(file.getPath()); // if the path is used it returns the path instead of the name of the //
+												// // file
+			System.out.println(file.getAbsolutePath()); // returns the location
+			System.out.println(file.isFile());
+			file.delete();
+		}
+		else {
+			System.out.println("The file doesn't exist");
+
+		}
+	}
+}
--- src/methodOverriding/Dog.java
+++ src/methodOverriding/Dog.java
@@ -3,7 +3,7 @@ package methodOverriding;
 public class Dog extends Animal {
 
 	@Override
-	void speak() {
+	public void speak() {
 		System.out.println("The dog goes *bark*");
 	}
 	// the method in the child class will be called instead of the same method
