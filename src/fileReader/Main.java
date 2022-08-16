71f98804ec79cf99f9b8a11a2ed77da2b2c734e5
menabaddo <edmundalexfresh@gmail.com>
2022-08-16 16:34:15 +0100

    File Reader

---
+++ src/fileReader/Main.java
@@ -0,0 +1,34 @@
+package fileReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// FileReader = read the contents of a file as a stream of characters. One by
+		// one
+		// read() returns an int value which contains the byte value when read() returns
+		// -1,
+		// there is no more data to be read.
+
+		try {
+			FileReader reader = new FileReader("poem.txt");
+			int data = reader.read();
+			System.out.println(data);
+
+			while (data != -1) {
+				System.out.print((char) data);
+				data = reader.read();
+			}
+
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File not found");
+
+		}
+
+	}
+
+}
