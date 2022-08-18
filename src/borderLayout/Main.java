3dc65bf085fb75d17d2809a0f268149b07a3609d
menabaddo <edmundalexfresh@gmail.com>
2022-08-18 20:03:48 +0100

    Border Layout

---
+++ src/borderLayout/Main.java
@@ -0,0 +1,82 @@
+package borderLayout;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Layout Manager = Defines the natural layout for components within a container
+
+		// 3 common managers
+
+		// BorderLayout = A BorderLayout places components in five areas:
+		// NORTH,SOUTH,WEST,EAST,CENTRE
+		// All extra space is placed in the center area.
+
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setLayout(new BorderLayout(10, 10)); // margins can also be passed here
+		frame.setVisible(true);
+
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		JPanel panel5 = new JPanel();
+
+		panel1.setBackground(Color.RED);
+		panel2.setBackground(Color.GREEN);
+		panel3.setBackground(Color.YELLOW);
+		panel4.setBackground(Color.MAGENTA);
+		panel5.setBackground(Color.BLUE);
+
+		panel1.setPreferredSize(new Dimension(100, 100));
+		panel2.setPreferredSize(new Dimension(100, 100));
+		panel3.setPreferredSize(new Dimension(100, 100));
+		panel4.setPreferredSize(new Dimension(100, 100));
+		panel5.setPreferredSize(new Dimension(100, 100));
+
+		// ==========this are sub panels=============================================
+		JPanel panel6 = new JPanel();
+		JPanel panel7 = new JPanel();
+		JPanel panel8 = new JPanel();
+		JPanel panel9 = new JPanel();
+		JPanel panel10 = new JPanel();
+
+		panel6.setBackground(Color.BLACK);
+		panel7.setBackground(Color.DARK_GRAY);
+		panel8.setBackground(Color.GRAY);
+		panel9.setBackground(Color.LIGHT_GRAY);
+		panel10.setBackground(Color.WHITE);
+
+		panel5.setLayout(new BorderLayout(10, 10));
+
+		panel6.setPreferredSize(new Dimension(50, 50));
+		panel7.setPreferredSize(new Dimension(50, 50));
+		panel8.setPreferredSize(new Dimension(50, 50));
+		panel9.setPreferredSize(new Dimension(50, 50));
+		panel10.setPreferredSize(new Dimension(50, 50));
+
+		panel5.add(panel6, BorderLayout.NORTH);
+		panel5.add(panel7, BorderLayout.SOUTH);
+		panel5.add(panel8, BorderLayout.WEST);
+		panel5.add(panel9, BorderLayout.EAST);
+		panel5.add(panel10, BorderLayout.CENTER);
+
+		frame.add(panel1, BorderLayout.NORTH);
+		frame.add(panel2, BorderLayout.WEST);
+		frame.add(panel3, BorderLayout.EAST);
+		frame.add(panel4, BorderLayout.SOUTH);
+		frame.add(panel5, BorderLayout.CENTER);
+
+	}
+
+}
